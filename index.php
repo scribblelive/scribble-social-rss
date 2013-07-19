40d143f5be02fc1c0b28b11447514241dd3fe0a2
Jonathan Keebler <jonathan@scribblelive.com>
2013-07-18 23:07:16 -0400

    Used twitter widgets for all posts that are tweets

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <link rel="stylesheet" href="https://app.divshot.com/css/bootstrap-responsive.css">
     <script src="https://app.divshot.com/js/jquery.min.js"></script>
     <script src="https://app.divshot.com/js/bootstrap.min.js"></script>
+    <script src="http://platform.twitter.com/widgets.js"></script>
+    
+
 </head>
 <body>
 <?php
@@ -20,20 +23,20 @@ $client = new Client('https://scribblelive.p.mashape.com/',
     )
 ));
 
-$request = $client->get('/event/39048/page/last?PageSize=10');
+$request = $client->get('/event/39048/page/last?PageSize=25');
 
 $json = $request->send()->json();
 
 $Title = $json["Title"];
 ?>
 <div class="container">
-      <h1 class="page-header"><?php echo $Title; ?>!</h1>
+      <h1 class="page-header"><?php echo $Title; ?></h1>
 
 <?php
 foreach( $json["Posts"] as $Post )
 {
 ?>
-    <div class="row" style="margin-bottom: 10px;">
+    <div class="row" style="margin-bottom: 10px;" id="Post<?php echo $Post["Id"] ?>">
         <div class="span2">
           <h4 style="text-align: right; margin-top: 0"><?php echo $Post["Creator"]["Name"]; ?></h4>
         </div>
@@ -44,6 +47,27 @@ foreach( $json["Posts"] as $Post )
           
           <p><?php echo $Post["Content"]; ?></p>
           
+          <?php
+          if( $Post["Source"] && preg_match( "/twitter\.com/", $Post["Source"] ) ) 
+          {
+              preg_match( "/[0-9]+/", $Post["Source"], $TweetIds );
+          }
+          else
+          {
+              $TweetIds = null;
+          }
+          
+          if( $TweetIds && count( $TweetIds ) )
+          {
+          ?>
+            <script>
+            twttr.ready( function(twttr) {
+                var TweetPost = $("#Post<?php echo $Post["Id"]; ?>");
+                TweetPost.find("div").empty();
+                twttr.widgets.createTweet('<?php echo $TweetIds[0]; ?>', TweetPost.find(".span8").get(0) );
+            })
+            </script>
+          <?php } ?>
         </div>
       </div>
       <hr />
